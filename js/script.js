5c24f0b794cfe47b77c26ff45d0e5bf2f4e67fef
WildAxewAppears <a.dafflitto@virgilio.it>
2024-04-30 12:18:47 +0200

    wrote logic behind how rounds works and corrected some typos

--- js/script.js
+++ js/script.js
@@ -14,10 +14,42 @@ let computerScore = 0
 const humanSelection = getHumanChoice()
 const computerSelection = getComputerChoice()
 
-function playRound (humanSelection, computerSelection) {
-   
+
+
+function playRound (humanChoice, computerChoice) {
+    if (humanSelection == "rock") {
+        if (computerSelection == 0) {
+            alert("You Both played Rock, you draw!")
+        }
+        else if (computerSelection ==1) {
+            alert("Unlucky Rock lose aganist Paper. You lose!")
+            computerScore++
+        }
+
+        else {
+            alert("Letsgoski! Rock wins aganist Scissors. You Win!")
+            humanScore++
+        }
+    }
+
+    if (humanSelection == "paper") {
+        if (computerSelection == 0) {
+            alert("Letsgoski! Paper wins aganist Rock. You Win!")
+            humanScore++
+        }
+        
+        else if (computerSelection == 1) {
+            alert("You Both played Paper, you draw!")
+            
+        }
+
+        else {
+            alert("Unlucky Paper lose aganist Scissors. You lose!")
+            computerScore++
+        }
+    }
+
 }
 
-
-console.log(playRound)
+playRound(humanSelection, computerSelection )
 
