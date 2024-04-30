af0088a831440a608ad208ea585879c2b3f92ce3
WildAxewAppears <a.dafflitto@virgilio.it>
2024-04-30 18:02:28 +0200

    Wrote the playGame function , added some details to not break the game and fixed some typos

--- js/script.js
+++ js/script.js
@@ -1,55 +1,102 @@
-function getComputerChoice() {
-    let computerChoice = Math.floor(Math.random() * 3);
-    return computerChoice
-}
 
-function getHumanChoice() {
-    let humanChoice = prompt("Enter Rock, Paper or Scissors to play!").toLowerCase();
-    return humanChoice
-}
+function playGame() {
 
-let humanScore = 0
-let computerScore = 0
+    humanScore = 0
+    computerScore = 0
 
-const humanSelection = getHumanChoice()
-const computerSelection = getComputerChoice()
-
-
-
-function playRound (humanChoice, computerChoice) {
-    if (humanSelection == "rock") {
-        if (computerSelection == 0) {
-            alert("You Both played Rock, you draw!")
-        }
-        else if (computerSelection ==1) {
-            alert("Unlucky Rock lose aganist Paper. You lose!")
-            computerScore++
-        }
-
-        else {
-            alert("Letsgoski! Rock wins aganist Scissors. You Win!")
-            humanScore++
-        }
+    while (humanScore < 5 && computerScore < 5) {
+        playRound();
     }
 
-    if (humanSelection == "paper") {
-        if (computerSelection == 0) {
-            alert("Letsgoski! Paper wins aganist Rock. You Win!")
-            humanScore++
+    if (humanScore >= 5) {
+        alert("Congratulations! You won the game!");
+    } else {
+        alert("Computer wins. Better luck next time!");
+    }
+
+    
+    function playRound () {
+
+        function getComputerChoice() {
+            let computerChoice = Math.floor(Math.random() * 3);
+            if (computerChoice == 0) {
+                return "rock"
+            }
+            else if (computerChoice == 1) {
+                return "paper"
+            }
+            else {
+                return "scissors"
+            }
         }
         
-        else if (computerSelection == 1) {
-            alert("You Both played Paper, you draw!")
-            
-        }
+        function getHumanChoice() {
+            let humanChoice = prompt("Enter Rock, Paper or Scissors to play!").toLowerCase();
 
-        else {
-            alert("Unlucky Paper lose aganist Scissors. You lose!")
-            computerScore++
+            if (humanChoice !== "rock"  && humanChoice !== "paper"  && humanChoice !== "scissors") {
+                alert("c'mon be serious...")
+                return getHumanChoice()
+            }
+            else
+            return humanChoice
         }
+    
+        const humanSelection = getHumanChoice()
+        
+        const computerSelection = getComputerChoice()
+        
+        if (humanSelection == "rock") {
+            if (computerSelection == "rock") {
+                alert("You Both played Rock, you draw!")
+            }
+            else if (computerSelection =="paper") {
+                alert("Unlucky Rock loses aganist Paper. You lose!")
+                computerScore++
+            }
+    
+            else {
+                alert("Letsgoski! Rock wins aganist Scissors. You Win!")
+                humanScore++
+            }
+        }
+    
+        if (humanSelection == "paper") {
+            if (computerSelection == "rock") {
+                alert("Letsgoski! Paper wins aganist Rock. You Win!")
+                humanScore++
+            }
+            
+            else if (computerSelection == "paper") {
+                alert("You Both played Paper, you draw!")
+                
+            }
+    
+            else {
+                alert("Unlucky Paper loses aganist Scissors. You lose!")
+                computerScore++
+            }
+        }
+    
+        if (humanSelection == "scissors") {
+            if (computerSelection == "rock") {
+                alert("Unlucky Scissors loses aganist Rock. You lose!")
+                computerScore++
+            }
+            
+            else if (computerSelection == "paper") {
+                alert("Letsgoski! Scissors wins aganist Paper. You Win!")
+                humanScore++
+                
+            }
+    
+            else {
+                alert("You Both played Scissors, you draw!")
+                
+            }
+        }
+    
     }
 
 }
 
-playRound(humanSelection, computerSelection )
-
+playGame()
